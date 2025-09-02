baef770551c2e32cc06841bc749be7cf3f0fb42a
Bélisle <2189722@cmontmorency.qc.ca>
2025-09-02 16:34:25 -0400

    Initialization

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <FastLED.h>
+#define MA_BROCHE_ANGLE 32
+
+CRGB pixelAtom;
+
+void setup() {
+  Serial.begin(115200);
+  FastLED.addLeds<WS2812, 27, GRB>(&pixelAtom, 1);
+}
+
+void loop() {
+  int maLectureAnalogique = analogRead(MA_BROCHE_ANGLE);
+  Serial.print(maLectureAnalogique);
+  Serial.println();
+  delay(100);
+
+  if (maLectureAnalogique > 4095 / 2) {
+    pixelAtom = CRGB(242,165,56);
+    FastLED.show();
+  } else {
+    pixelAtom = CRGB(0,0,0);
+    FastLED.show();
+  }
+}
